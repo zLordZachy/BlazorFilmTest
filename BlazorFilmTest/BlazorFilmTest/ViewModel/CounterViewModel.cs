0f8ad6b26b114000c0720d864ca040957c85c161
DESKTOP-NTPQU59\Tomas <zLordZachy@gmail.com>
2021-01-24 14:11:48 +0100

    uvodní commit

---
+++ BlazorFilmTest/BlazorFilmTest/ViewModel/CounterViewModel.cs
@@ -0,0 +1,20 @@
+﻿
+namespace BlazorFilmTest.ViewModel
+{
+    public class CounterViewModel
+    {
+
+        public int CurrentCount { get; set; }
+
+        public CounterViewModel()
+        {
+            CurrentCount = 0;
+        }
+
+        public void IncrementCount()
+        {
+            CurrentCount++;
+        }
+
+    }
+}
